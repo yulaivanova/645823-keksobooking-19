aa6e46318096f9cd02524319fcb9969da1e9f581
Yula Ivanova <yulialepehina@gmail.com>
2020-02-17 15:10:38 +0300

    Отрисовка карточки через замыкание, добавила движение метки по карте

--- js/map.js
+++ js/map.js
@@ -6,6 +6,10 @@
   var ESC_KEY = 'Escape';
   var PIN_QUANTITY = 8;
 
+  var PIN_MAIN_WIDTH = 65;
+  var PIN_MAIN_HEIGHT = 65;
+  var PIN_MAIN_ARROW = 16;
+
   var map = document.querySelector('.map');
   var mapPinsList = map.querySelector('.map__pins');
   var adForm = document.querySelector('.ad-form');
@@ -17,6 +21,8 @@
   var mapFormSelects = mapFiltersContainer.querySelectorAll('.map__filters-container select');
   var mapPinMain = map.querySelector('.map__pin--main');
 
+  var MAP_WIDTH = map.offsetWidth;
+  var MAP_HEIGHT = map.offsetHeight - mapFilters.offsetHeight;
 
   var generatedPins = window.pin.createElements(PIN_QUANTITY);
 
@@ -34,11 +40,31 @@
   var renderPins = function (pins) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < pins.length; i++) {
-      fragment.appendChild(window.pin.createElement(pins[i]));
+      var element = window.pin.createElement(pins[i]);
+      var clickPinHandler = createClickPinHandler(i);
+      element.addEventListener('click', clickPinHandler);
+      element.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === KEY_ENTER) {
+          clickPinHandler();
+        }
+      });
+      fragment.appendChild(element);
     }
     mapPinsList.appendChild(fragment);
   };
 
+  var createClickPinHandler = function (index) {
+    var clickPinHandler = function () {
+      var pinCard = map.querySelector('.map__card');
+      if (pinCard) {
+        pinCard.remove();
+        renderPinCard(generatedPins[index]);
+      } else {
+        renderPinCard(generatedPins[index]);
+      }
+    };
+    return clickPinHandler;
+  };
 
   var renderPinCard = function (pin) {
     map.insertBefore(window.pinCard.createElement(pin), mapFilters);
@@ -59,31 +85,6 @@
     window.form.toogleElements(adFormFieldsets, false);
     window.form.updateAddressInput(adFromAddressInput, true);
     renderPins(generatedPins);
-
-    var pins = map.querySelectorAll('.map__pin:not(.map__pin--main)');
-    pins.forEach(function (pin, i) {
-      pin.addEventListener('click', function () {
-        var pinCard = map.querySelector('.map__card');
-        if (pinCard) {
-          pinCard.remove();
-          renderPinCard(generatedPins[i]);
-        } else {
-          renderPinCard(generatedPins[i]);
-        }
-      });
-
-      pin.addEventListener('keydown', function (evt) {
-        if (evt.key === KEY_ENTER) {
-          var pinCard = map.querySelector('.map__card');
-          if (pinCard) {
-            pinCard.remove();
-            renderPinCard(generatedPins[i]);
-          } else {
-            renderPinCard(generatedPins[i]);
-          }
-        }
-      });
-    });
   };
 
   mapPinMain.addEventListener('click', function () {
@@ -94,6 +95,63 @@
     if (evt.keyCode === KEY_ENTER) {
       makePageActive();
     }
+  });
+
+  mapPinMain.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var dragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      var offsetLeft = mapPinMain.offsetLeft - shift.x;
+      var offsetTop = mapPinMain.offsetTop - shift.y;
+
+      if (offsetLeft < MAP_WIDTH - PIN_MAIN_WIDTH && offsetLeft > 0 && offsetTop > PIN_MAIN_HEIGHT && offsetTop < MAP_HEIGHT - PIN_MAIN_HEIGHT - PIN_MAIN_ARROW) {
+        mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+        mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+      }
+
+      window.form.updateAddressInput(adFromAddressInput, true);
+
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          mapPinMain.removeEventListener('click', onClickPreventDefault);
+        };
+        mapPinMain.addEventListener('click', onClickPreventDefault);
+      }
+
+      makePageActive();
+
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
   });
 
   window.form.toogleElements(mapFormInputs, true);
